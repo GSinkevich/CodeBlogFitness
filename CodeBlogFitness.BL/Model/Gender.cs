a27380460a11e936c6e6ee768aa1c609e82c2607
krafpage@gmail.com <krafpage@gmail.com>
2020-05-21 14:27:40 +0300

    #2 Create Users( Properties and chek
    if (){ throw new .... ,nameof(  ));

---
+++ CodeBlogFitness.BL/Model/Gender.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness.BL.Model
+{
+    public class Gender
+    {
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Создание нового пользователя 
+        /// </summary>
+        /// <param name="name"></param>
+        public Gender (string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Введите корректное имя пользователя",nameof(name));
+            }
+
+            Name = name;
+                        
+        }
+
+        public override string ToString()
+        {
+            return Name ;
+        }
+
+    }
+}
---
+++ CodeBlogFitness.BL/Model/User.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data;
+
+namespace CodeBlogFitness.BL.Model
+{
+    public class User
+    {
+        #region Свойства Пользователя 
+        public string Name { get; }
+
+        public Gender Gender { get; }
+          
+        public DateTime BirthDate { get; }
+
+        public double Weight { get; set; }
+
+        public double Height  { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Создание пользователя
+        /// </summary>
+        /// <param name="name">Имя.</param>
+        /// <param name="gender">Пол.</param>
+        /// <param name="birthDate">Дата Рождения</param>
+        /// <param name="weight">Вес.</param>
+        /// <param name="height">Рост.</param>
+        public User(string name,
+                        Gender gender,
+                        DateTime birthDate,
+                        double weight,
+                        double height)
+        {
+    #region Проверка условий
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пользоватля не может быть пустым ", nameof(name));
+            }
+
+            if (Gender == null)
+            {
+                throw new ArgumentNullException("Пол Не может быть кривым", nameof(gender));
+            }
+
+            if (birthDate < DateTime.Parse("01.01.1900") || birthDate >= DateTime.Now)
+            {
+                throw new ArgumentException("невозможная дата рождения", nameof(birthDate)); 
+            }
+
+            if (weight<=0)
+            {
+                throw new ArgumentNullException("Вес не может быть меньше 0",nameof(weight));
+            }
+
+            if (height <= 0)
+            {
+                throw new ArgumentNullException("Рост не может быть меньше 0", nameof(height));
+            }
+            #endregion
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
